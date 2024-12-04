311d211f8dd19dac3e1d19322a71d0321e0425b0
orianodev <nono5g@protonmail>
2024-12-04 09:08:40 +0100

    intro API

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express = require('express');;
+const express = require('express');
 const { join } = require('node:path');
 const app = express();
 const { createServer } = require('node:http');
 const server = createServer(app);
 
 app.use(express.static(join(__dirname)));
+
+app.get("/test", function (req, res) {
+  res.send("🐔")
+})
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
