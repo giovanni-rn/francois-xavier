8424d5c62d578f4c5930bbc51f0cf65d300a6d26
orianodev <nono5g@protonmail>
2024-12-13 09:05:16 +0100

    codage de api post signup-user

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ const app = express();
 const { createServer } = require('node:http');
 const server = createServer(app);
 
-app.use(express.static(join(__dirname)));
+app.use(express.static(join(__dirname))); // ex: http://localhost:3000/signup.html
 
 app.get("/test", function (req, res) {
   res.send("🐔")
+});
+
+// ex: http://localhost:3000/signup-user
+app.post("/signup-user", function (req, res) {
+  // req.body = {
+  //   username: username,
+  //   pasword: password,
+  //   email: email,
+  // };
+  req.body
 })
 
 server.listen(3000, () => {
