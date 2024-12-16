8e9f39700812204d4dec6adc29c1630a9bd4b8e1
orianodev <nono5g@protonmail>
2024-12-16 09:09:03 +0100

    fin api post

--- server.js
+++ server.js
@@ -4,21 +4,19 @@ const app = express();
 const { createServer } = require('node:http');
 const server = createServer(app);
 
-app.use(express.static(join(__dirname))); // ex: http://localhost:3000/signup.html
+app.use(express.static(join(__dirname))); // ex: http://localhost:3000/*.html
+app.use(express.json()) // Traiter les données au format JSON
 
+// ex: http://localhost:3000/test
 app.get("/test", function (req, res) {
   res.send("🐔")
 });
 
 // ex: http://localhost:3000/signup-user
 app.post("/signup-user", function (req, res) {
-  // req.body = {
-  //   username: username,
-  //   pasword: password,
-  //   email: email,
-  // };
-  req.body
-})
+  console.log(req.originalUrl, req.method, req.headers, req.ip, req.host, req.path, req.statusCode, req.body);
+  res.status(200).json({ "Message": "User created" }) // Renvoyer un message de confirmation au navigateur
+});
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
