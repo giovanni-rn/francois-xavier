e5324725c305e0492b4410ada61904e3fed422e3
orianodev <nono7g@proton.me>
2025-01-27 09:07:21 +0100

    elementaire python

---
+++ python/elementaire.js
@@ -0,0 +1,13 @@
+// Commentaire...
+
+var viande1 = "poulet" // ancienne (obsolète)
+let viande2 = "poulet" // modifiable
+const viande3 = "poulet" // constante
+viande2 = "dinde"
+
+function add(x, y) {
+    return x + y
+}
+
+// add(1, 2) // calcule 3 mais n'affiche rien
+console.log(add(1, 2)); // calcule 3 et affiche 3 dans le terminal
---
+++ python/elementaire.py
@@ -0,0 +1,9 @@
+# Commentaire...
+
+viande1 = "poulet"
+viande1 = "dinde"
+
+def add(x, y):
+    return x + y
+
+add(1, 2)
